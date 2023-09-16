10e22b0409c20c8bb974f63e8efa462e13e9d58b
rezzak ali <rezzak ali>
2023-09-16 14:28:38 +0530

    server setup code push with error handling

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import colors from 'colors';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import errorHandler from '../middlewares/errorHandler.js';
 
 // configure environment
 dotenv.config();
@@ -16,6 +17,10 @@ colors.enable();
 // app instance
 const app = express();
 
+// body-parser
+app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
+
 // cors policy
 app.use(
   cors({
@@ -29,9 +34,20 @@ app.get('/api/v1/test', (req, res) => {
   res.send('Hello Developers!');
 });
 
+// custom error handler
+app.use(errorHandler);
+
+// error handler
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next();
+  }
+  res.status(500).json({ error: 'There was a server side error!' });
+});
+
 // listening server
 app.listen(PORT, HOST_NAME, () => {
   console.log(
-    `🚀 Your server is running successfully on http://${HOST_NAME}:${PORT}`
+    `🚀 Your server is running successfully on http://${HOST_NAME}:${PORT}`.blue
   );
 });
